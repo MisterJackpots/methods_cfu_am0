60f94d6f52b7f70a51064cc6623719c1b40bd8fc
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-07-12 15:36:50 -0700

    Add answers to problems 1,2

--- final_practice.rb
+++ final_practice.rb
@@ -1,18 +1,30 @@
 # Defining Our Own Methods
 # 1: Write a method named greeting that prints out a general greeting to someone
+def greeting
+  puts "Hello, old friend!"
+  puts "It's good to see you again, I hope you've been well."
+end
 
 # What is the return value of your method?
+# The return value is nil
 # How many arguments did you pass your method?
+# There were no arguments passed
 
 
 
 
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
 
+def custom_greeting
+  puts "Welcome, Gabe!"
+end
 
 # What is the return value of your method?
+# The return value was nil
 # How many arguments did you pass your method?
+# There were no arguments passed
 # What data type was your argument(s)?
+# The data type was a string
 
 
 
@@ -35,4 +47,3 @@
 # What is the return value of your method?
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
-
